cf1320fe7562d9e2fd23fb92c11f6bb77838acd6
himani-s993 <himanis993@gmail.com>
2021-11-26 19:44:54 +0530

    non-keyword(*) and keyword(**) arguments

--- functions/first.py
+++ functions/first.py
@@ -3,3 +3,21 @@
 def someFunction(someParam):
     print("Some Value ", someParam)
 someFunction("Yeah")
+
+
+# Variable Length Arguments => *non-keyword and **keyword arguments
+def func1(*arguments):
+    for i in arguments:
+        print("Argument -> ", i)
+func1(1, 2, 3)
+func1([1, 2, 3])
+
+def func2(**args):
+    for k, v in args.items():
+        print("Key %s Value %s " %(k, v))
+func2(a="1",b="2",c=3)
+
+
+
+
+
