290900d98764d5069ccf541f2201ac88998cd40b
himani-s993 <himanis993@gmail.com>
2021-11-28 17:03:03 +0530

    Specific Exception handling and raise exception

--- exception-handling/first.py
+++ exception-handling/first.py
@@ -28,3 +28,13 @@ def AbyB(a, b):
 # Driver program to test above function
 AbyB(2.0, 3.0)
 AbyB(3.0, 3.0)
+
+
+# 3. Specifying Exceptions and raising Exception s
+def func2():
+    try:
+        raise NameError
+    except NameError:
+        print("raised and handled")
+
+func2()
