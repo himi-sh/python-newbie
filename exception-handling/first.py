feafdffafc1dd08cd19482f7354ceae858f65d1a
himani-s993 <himanis993@gmail.com>
2021-11-28 17:00:39 +0530

    try, catch, else

--- exception-handling/first.py
+++ exception-handling/first.py
@@ -5,12 +5,26 @@
 
 def funct1(a, b):
     try:
+        d = 1
         return a / b
     except:
         print("Exception occurred")
     finally:
         print("Exceution done - finally")
 
-print("1st =>",funct1(1,2))
-print("2nd =>", funct1(1,0))
+print("1st =>", funct1(1, 2))
+print("2nd =>", funct1(1, 0))
 
+# 2. Eg 2
+
+def AbyB(a, b):
+    try:
+        c = ((a + b) / (a - b))
+    except ZeroDivisionError:
+        print("a/b result in 0")
+    else:
+        print("c = ", c)
+
+# Driver program to test above function
+AbyB(2.0, 3.0)
+AbyB(3.0, 3.0)
