52698b288934070062bc820ff239ecf47d600ce1
himani-s993 <himanis993@gmail.com>
2021-11-28 16:54:10 +0530

    Exception occurred

---
+++ exception-handling/first.py
@@ -0,0 +1,12 @@
+# Exception class hierrachy
+# https://docs.python.org/2/library/exceptions.html#exception-hierarchy
+
+# 1. Eg 1
+
+def funct1(a, b):
+        return a / b
+
+
+print(funct1(1,2))
+print(funct1(1,0))
+
